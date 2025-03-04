7f0e4fb7bef35866689418f225bce8b06dd375d7
kspica <s15990@pjwstk.edu.pl>
2025-03-04 14:24:38 +0100

    Create project using Spting Boot Initializer.

---
+++ pjaidApp/src/main/java/org/api/pjaidapp/PjaidAppApplication.java
@@ -0,0 +1,13 @@
+package org.api.pjaidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjaidAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjaidAppApplication.class, args);
+    }
+
+}
---
+++ pjaidApp/src/test/java/org/api/pjaidapp/PjaidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.pjaidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjaidAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
