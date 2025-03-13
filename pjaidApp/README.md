3a67e099d97f567f27e6acba0538461df49b31b9
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-03-13 15:13:48 +0100

    Usunięcie .idea, .mvn, .vscode z repozytorium

--- pjaidApp/README.md
+++ pjaidApp/README.md
@@ -4,4 +4,3 @@ To start a local development server, run:
 ```bash
 ng serve
 ```
-
--- pjaidApp/src/main/angular/app/main.ts
+++ pjaidApp/src/main/angular/app/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './angular/app.config';
-import { AppComponent } from './angular/app.component';
+import { appConfig } from '../app.config';
+import { AppComponent } from '../app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
