22a20f05eff01aa367c514d5886618113de1739d
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-04-04 11:49:42 +0200

    [WEB] Frontend Lista zgłoszeń wygenerowana przez kom. ng generate component components/ticket-list

---
+++ pjaidApp/backend/pjaidJavaApp/frontend/src/app/components/ticket-list/ticket-list.component.ts
@@ -0,0 +1,22 @@
+// import { Component, OnInit } from '@angular/core';
+// import { TicketService, Ticket } from '../../ticket.service';
+// import { NgForOf } from '@angular/common';
+// import { RouterLink } from '@angular/router';
+//
+// @Component({
+//   selector: 'app-ticket-list',
+//   standalone: true,
+//   imports: [NgForOf, RouterLink],
+//   templateUrl: './ticket-list.component.html',
+//   styleUrls: ['./ticket-list.component.css']
+// })
+// export class TicketListComponent implements OnInit {
+//   tickets: Ticket[] = [];
+
+  // constructor(private ticketService: TicketService) {}
+  //
+  // ngOnInit(): void {({{{}
+  //   this.ticketService.// tu dajemy nazwe beckendu z pliku TicketControler() Adrian jak bedziesz miał to możesz dodać.subscribe({
+  //
+  // }
+// }
