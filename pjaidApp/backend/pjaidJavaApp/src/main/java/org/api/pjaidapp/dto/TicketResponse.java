8c84a4ab68e8296d59cbd9d035a3324024a0310b
s22620 <s22620@pjwstk.edu.pl>
2025-04-05 20:12:47 +0200

    [SCRUM - 57] - [WEB] Backend edycja statusu zgłoszenia

--- pjaidApp/backend/pjaidJavaApp/src/main/java/org/api/pjaidapp/dto/TicketResponse.java
+++ pjaidApp/backend/pjaidJavaApp/src/main/java/org/api/pjaidapp/dto/TicketResponse.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TicketResponse {
-    private int id;
+    private Long id;
     private String title;
     private String description;
     private String status;
--- pjaidApp/backend/pjaidJavaApp/src/main/java/org/api/pjaidapp/dto/TicketStatusUpdateDTO.java
+++ pjaidApp/backend/pjaidJavaApp/src/main/java/org/api/pjaidapp/dto/TicketStatusUpdateDTO.java
@@ -5,12 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TicketStatusUpdateDTO {
-    private Integer id;
+    private Long id;
     private String status;
     private String comment;
 }
