d5251d81578745230e56e172535d00d800411d2d
Adrian Goik <adekgoik58@gmail.com>
2025-04-19 23:07:14 +0200

    SCRUM-77-IOS Formularz Zgłoszenia Awarii

---
+++ pjaidMobileiOS/pjaidMobileiOSTests/pjaidMobileiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  pjaidMobileiOSTests.swift
+//  pjaidMobileiOSTests
+//
+//  Created by Adrian Goik on 19/04/2025.
+//
+
+import Testing
+@testable import pjaidMobileiOS
+
+struct pjaidMobileiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
