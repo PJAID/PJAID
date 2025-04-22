2326bf67325d3a892b11c581235aefefb75998eb
s25866-pj <s25866@pjwstk.edu.pl>
2025-04-22 19:01:04 +0200

    SCRUM-118 [WEB] - Frontend, poprawki rejestracji awarii

--- pjaidApp/backend/pjaidJavaApp/frontend/src/app/tickets/ticket-new/ticket-new.component.ts
+++ pjaidApp/backend/pjaidJavaApp/frontend/src/app/tickets/ticket-new/ticket-new.component.ts
@@ -6,11 +6,17 @@ import {Router} from '@angular/router';
 import {Device} from '../../shared/models/device.model';
 import {TicketRequest} from '../../shared/models/ticket-request.model';
 import {DeviceService} from '../../devices/services/device.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {map, Observable, startWith} from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ticket-new',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, FormsModule],
   templateUrl: './ticket-new.component.html',
   styleUrl: './ticket-new.component.css',
   providers: [TicketService, DeviceService]
@@ -23,7 +29,8 @@ export class TicketNewComponent implements OnInit {
 
   devices: Device[] = [];
   userId: number = 1;
-
+  deviceControl = this.fb.control<Device | null>(null,Validators.required);
+  filteredDevices!: Observable<Device[]>;
   ticketForm = this.fb.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
@@ -34,9 +41,19 @@ export class TicketNewComponent implements OnInit {
   ngOnInit(): void {
     this.deviceService.getDevices().subscribe(devices => {
       this.devices = devices;
+      this.filteredDevices = this.deviceControl.valueChanges.pipe(
+        startWith(''),
+        map(value => {
+          const filterValue = typeof value === 'string' ? value : value?.id?.toString() ?? '';
+          return this.devices.filter(device =>device.id.toString().includes(filterValue));
+        })
+      )
     });
-  }
 
+  }
+  displayFn(device: Device): string {
+    return device ? `${device.id} ${device.name} (${device.serialNumber})` : '';
+  }
   submit() {
     if (this.ticketForm.valid) {
       const formValue = this.ticketForm.getRawValue();
